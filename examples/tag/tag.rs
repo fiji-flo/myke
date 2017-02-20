49a6076baf562dcdff2fa73070e70fa0c6c32697
Florian Merz <florian.merz@goeuro.com>
2017-02-20 19:25:23 +0100

    tags tests update

--- examples/tag/tag.rs
+++ examples/tag/tag.rs
@@ -3,6 +3,13 @@ myke_test_file!();
 myke_test!(
     name tag;
     cd "examples/tag";
-    "myke tagA/tag" => r"tags1 tag\n.*";
-    "myke tagC/tag" => r"tags2 tag\n.*";
+    "myke tagA/tag" => "tags1 tag";
+    "myke tagC/tag" => "tags2 tag";
+//    "myke --dry-run tag" => "(?s)tags1/tag: Will run.*tags2/tag: Will run";
+    "myke tagA/tag" => "tags1 tag";
+    "myke tagA/tag" => "(tags2){0}";
+    "myke tagB/tag" => "tags1/tag";
+    "myke tagB/tag" => "tags2/tag";
+    "myke tagC/tag" => "(tags1){0}";
+    "myke tagC/tag" => "tags2 tag";
 );
