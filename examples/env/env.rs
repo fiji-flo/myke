c8856489b6de41965c2473b7f0e6e1ae2dfca9d9
Florian Merz <florian.merz@goeuro.com>
2017-03-13 15:58:50 +0100

    fix env test for path

--- examples/env/env.rs
+++ examples/env/env.rs
@@ -10,5 +10,5 @@ myke_test!(
 	  "myke file_custom" => r"envvar from test.env is value_from_test.env";
 	  "myke file_custom_local" => r"envvar from test.env.local is value_from_test.env.local";
     // myke-rs expandes PATH elements
-	  "myke path" => r"PATH is [^:]+.*/env/path_from_myke.env.local:[^:]+.*/env/path_from_myke.env:[^:]+.*/env/path_from_test.env.local:[^:]+.*/env/path_from_test.env:[^:]+.*/env/path_from_yml:[^:]+.*/env/bin";
+	  "myke path" => r"PATH is [^:;]+.*path_from_myke.env.local:[^:;]+.*path_from_myke.env:[^:;]+.*path_from_test.env.local:[^:;]+.*path_from_test.env:[^:;]+.*path_from_yml:[^:]+.*bin";
 );
